0e2230d839801681ff579b99299b14ca6b9d67b8
Juan Hernandez <hernanjj@mail.gvsu.edu>
2023-12-04 11:24:14 -0500

    Updates1(12/4/23)
    Changed hitboxes
    Created enemy spawner class
    Added cave.png

---
+++ Actual/enemyspawner.py
@@ -0,0 +1,15 @@
+import os
+import pygame as pg
+import random
+
+class EnemySpawner(pg.sprite.Sprite):
+    def __init__(self):
+        super (EnemySpawner, self).__init__()
+        self.image = pg.image.load(os.path.join('assets', 'cave.png')).convert_alpha()
+        IMAGE_SIZE = (125,125)
+        self.image = pg.transform.scale(self.image, IMAGE_SIZE)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = 100
+        self.rect.centery = 350
+
+        
